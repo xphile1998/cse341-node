49452c40fbf0f4fdb8987731b0f76091d85dcf94
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2023-01-05 12:54:36 -0800

    Seeing if I can use my laptop

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+    res.send('Annette Whitefield');
+});
+routes.get('/eron', (req, res) => {
+    res.send('Eron Whitefield');
+});
+routes.get('/jack', (req, res) => {
+    res.send('Jack Whitefield');
+});
+
+module.export = routes;
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use('/', require('./routes'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
