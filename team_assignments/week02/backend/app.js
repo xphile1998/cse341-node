1ddef4bdd8e62cdbd866ac9f8fdbead5ae62e5c3
xphile1998 <eron.whitefield@gmail.com>
2023-01-10 18:10:05 -0800

    Setup for Team Assignment Week 02

--- team_assignments/week02/backend/app.js
+++ team_assignments/week02/backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'))
+app.use('/', require('./routes/professional'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
