a3e3dbab7c70382bdefb179a9d86b1b948f76091
xphile1998 <eron.whitefield@gmail.com>
2023-01-14 15:10:24 -0800

    Starting over - Personal Assignment

--- personal_assignments/week02/routes/index.js
+++ personal_assignments/week02/routes/index.js
@@ -1,28 +1,6 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-routes.get('/', (req, res) => {
-    res.send('Annette Whitefield');
-});
-routes.get('/eron', (req, res) => {
-    res.send('Eron Whitefield');
-});
-routes.get('/jack', (req, res) => {
-    res.send('Jack Whitefield');
-});
-routes.get('/thomas', (req, res) => {
-    res.send('Thomas Whitefield');
-});
-routes.get('/robert', (req, res) => {
-    res.send('Robert Whitefield');
-});
-routes.get('/beth', (req, res) => {
-    res.send('Elisabeth Whitefield');
-});
-routes.get('/patrick', (req, res) => {
-    res.send('Patrick Whitefield');
-});
-routes.get('/week02', (req, res) => {
-    res.send('Week 2 Team Assignment Placeholder');
-});
+router.use('./contacts.js', require('./contacts'))
 
-module.exports = routes;
+module.exports = router;
