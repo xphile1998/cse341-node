5658799c64dff613efbb5ce22a23c34dcb84dad5
xphile1998 <eron.whitefield@gmail.com>
2023-01-20 14:12:38 -0800

    changed file name

--- personal_assignments/week02/routes/contacts.js
+++ personal_assignments/week02/routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const contactsController = require("../controllers/contacts");
+const contactsController = require("../controllers/user");
 
 router.get("/", contactsController.getAll);
 
