5bed31bcaba47919b249190054b183f69f411a9b
xphile1998 <eron.whitefield@gmail.com>
2023-01-21 11:18:07 -0800

    Updated DELETE routine with deleteOne instead of remove

--- personal_assignments/week02/controllers/contacts.js
+++ personal_assignments/week02/controllers/contacts.js
@@ -56,7 +56,7 @@ const updateContact = async (req, res) => {
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
 
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
