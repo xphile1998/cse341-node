a2204d4792b1ccbca503f152077faadfb966dba5
xphile1998 <eron.whitefield@gmail.com>
2023-02-03 22:25:39 -0800

    edited controllers/users.js

--- personal_assignments/week05-08/controllers/users.js
+++ personal_assignments/week05-08/controllers/users.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db('cookbook').collection('users').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -11,22 +11,27 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+  const result = await mongodb.getDb().db().collection('users').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
 };
 
-const createContact = async (req, res) => {
-  const contact = {
+const createUser = async (req, res) => {
+  const user = {
+    userName: req.body.userName,
+    password: req.body.password,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    emailAddress: req.body.emailAddress,
+    preferredLanguage: req.body.preferredLanguage,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country,
+    public: req.body.public
   };
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  const response = await mongodb.getDb().db().collection('users').insertOne(user);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
@@ -34,17 +39,22 @@ const createContact = async (req, res) => {
   }
 };
 
-const updateContact = async (req, res) => {
+const updateUser = async (req, res) => {
   const userId = new ObjectId(req.params.id);
 
-  const contact = {
+  const user = {
+    userName: req.body.userName,
+    password: req.body.password,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    emailAddress: req.body.emailAddress,
+    preferredLanguage: req.body.preferredLanguage,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country,
+    public: req.body.public
   };
-  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId }, contact);
+  const response = await mongodb.getDb().db().collection('users').replaceOne({ _id: userId }, user);
   console.log(response);
   if (response.modifiedCount > 0) {
     res.status(204).send();
@@ -53,10 +63,10 @@ const updateContact = async (req, res) => {
   }
 };
 
-const deleteContact = async (req, res) => {
+const deleteUser = async (req, res) => {
   const userId = new ObjectId(req.params.id);
 
-  const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId }, true);
+  const response = await mongodb.getDb().db().collection('users').deleteOne({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
@@ -68,7 +78,7 @@ const deleteContact = async (req, res) => {
 module.exports = { 
   getAll, 
   getSingle, 
-  createContact,
-  updateContact, 
-  deleteContact
+  createUser,
+  updateUser, 
+  deleteUser
 };
--- personal_assignments/week05-08/routes/index.js
+++ personal_assignments/week05-08/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/recipes', require('./recipes'));
+router.use('/users', require('./users'))
 
 module.exports = router;
