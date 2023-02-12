c18ac77b779dc0e1c94f6d512651ab724e2f996f
xphile1998 <eron.whitefield@gmail.com>
2023-02-11 20:03:29 -0800

    help!

--- personal_assignments/week05-08/routes/validate.js
+++ personal_assignments/week05-08/routes/validate.js
@@ -8,10 +8,10 @@ router.get('/', userController.getAll);
 
 router.get('/:id', userController.getSingle);
 
-router.post('/', validation.saveContact, userController.createContact);
+router.post('/', validation.saveUser, userController.createUser);
 
-router.put('/:id', validation.saveContact, userController.updateContact);
+router.put('/:id', validation.saveUser, userController.updateUser);
 
-router.delete('/:id', userController.deleteContact);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
